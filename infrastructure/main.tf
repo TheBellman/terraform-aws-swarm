22d5cb5e8bee5ed3bce2689691ede992736e3378
Robert Hook <rahook@gmail.com>
2019-03-05 11:22:30 +0000

    tweak NACL

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -60,10 +60,20 @@ resource "aws_network_acl" "public" {
   tags       = "${merge(map("Name", "${var.namespace}-public"), var.tags)}"
 }
 
+resource "aws_network_acl_rule" "public_ephemeral_out" {
+  network_acl_id = "${aws_network_acl.public.id}"
+  rule_number    = 100
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 1024
+  to_port        = 65535
+}
 
 resource "aws_network_acl_rule" "public_http_out" {
   network_acl_id = "${aws_network_acl.public.id}"
-  rule_number    = 100
+  rule_number    = 101
   egress         = true
   protocol       = "tcp"
   rule_action    = "allow"
@@ -74,24 +84,13 @@ resource "aws_network_acl_rule" "public_http_out" {
 
 resource "aws_network_acl_rule" "public_https_out" {
   network_acl_id = "${aws_network_acl.public.id}"
-  rule_number    = 101
+  rule_number    = 102
   egress         = true
   protocol       = "tcp"
   rule_action    = "allow"
   cidr_block     = "0.0.0.0/0"
   from_port      = 443
   to_port        = 443
-}
-
-resource "aws_network_acl_rule" "public_ephemeral_out" {
-  network_acl_id = "${aws_network_acl.public.id}"
-  rule_number    = 110
-  egress         = true
-  protocol       = "tcp"
-  rule_action    = "allow"
-  cidr_block     = "0.0.0.0/0"
-  from_port      = 1024
-  to_port        = 65535
 }
 
 resource "aws_network_acl_rule" "public_ephemeral_in" {
@@ -105,6 +104,17 @@ resource "aws_network_acl_rule" "public_ephemeral_in" {
   to_port        = 65535
 }
 
+resource "aws_network_acl_rule" "public_http_in" {
+  network_acl_id = "${aws_network_acl.public.id}"
+  rule_number    = 101
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 80
+  to_port        = 80
+}
+
 resource "aws_network_acl_rule" "public_https_in" {
   network_acl_id = "${aws_network_acl.public.id}"
   rule_number    = 102
@@ -116,15 +126,94 @@ resource "aws_network_acl_rule" "public_https_in" {
   to_port        = 443
 }
 
-resource "aws_network_acl_rule" "public_http_in" {
+# Docker needs tcp 2377 and 7946, and udp 7946 and 4789
+# between each node in the swarm
+resource "aws_network_acl_rule" "public_docker_in_1" {
   network_acl_id = "${aws_network_acl.public.id}"
-  rule_number    = 111
+  rule_number    = 200
   egress         = false
   protocol       = "tcp"
   rule_action    = "allow"
   cidr_block     = "${aws_subnet.private.cidr_block}"
-  from_port      = 80
-  to_port        = 80
+  from_port      = 2377
+  to_port        = 2377
+}
+
+resource "aws_network_acl_rule" "public_docker_in_2" {
+  network_acl_id = "${aws_network_acl.public.id}"
+  rule_number    = 201
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.private.cidr_block}"
+  from_port      = 7946
+  to_port        = 7946
+}
+
+resource "aws_network_acl_rule" "public_docker_in_3" {
+  network_acl_id = "${aws_network_acl.public.id}"
+  rule_number    = 202
+  egress         = false
+  protocol       = "udp"
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.private.cidr_block}"
+  from_port      = 7946
+  to_port        = 7946
+}
+
+resource "aws_network_acl_rule" "public_docker_in_4" {
+  network_acl_id = "${aws_network_acl.public.id}"
+  rule_number    = 203
+  egress         = false
+  protocol       = "udp"
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.private.cidr_block}"
+  from_port      = 4789
+  to_port        = 4789
+}
+
+resource "aws_network_acl_rule" "public_docker_out_1" {
+  network_acl_id = "${aws_network_acl.public.id}"
+  rule_number    = 200
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.private.cidr_block}"
+  from_port      = 2377
+  to_port        = 2377
+}
+
+resource "aws_network_acl_rule" "public_docker_out_2" {
+  network_acl_id = "${aws_network_acl.public.id}"
+  rule_number    = 201
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.private.cidr_block}"
+  from_port      = 7946
+  to_port        = 7946
+}
+
+resource "aws_network_acl_rule" "public_docker_out_3" {
+  network_acl_id = "${aws_network_acl.public.id}"
+  rule_number    = 202
+  egress         = true
+  protocol       = "udp"
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.private.cidr_block}"
+  from_port      = 7946
+  to_port        = 7946
+}
+
+resource "aws_network_acl_rule" "public_docker_out_4" {
+  network_acl_id = "${aws_network_acl.public.id}"
+  rule_number    = 203
+  egress         = true
+  protocol       = "udp"
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.private.cidr_block}"
+  from_port      = 4789
+  to_port        = 4789
 }
 
 # ------------------------------------------------------------------------------
@@ -175,9 +264,20 @@ resource "aws_network_acl" "private" {
   tags       = "${merge(map("Name", "${var.namespace}-private"), var.tags)}"
 }
 
-resource "aws_network_acl_rule" "private_http_out" {
+resource "aws_network_acl_rule" "ephemeral_out" {
   network_acl_id = "${aws_network_acl.private.id}"
   rule_number    = 100
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 1024
+  to_port        = 65535
+}
+
+resource "aws_network_acl_rule" "private_http_out" {
+  network_acl_id = "${aws_network_acl.private.id}"
+  rule_number    = 101
   egress         = true
   protocol       = "tcp"
   rule_action    = "allow"
@@ -188,24 +288,13 @@ resource "aws_network_acl_rule" "private_http_out" {
 
 resource "aws_network_acl_rule" "private_https_out" {
   network_acl_id = "${aws_network_acl.private.id}"
-  rule_number    = 101
+  rule_number    = 102
   egress         = true
   protocol       = "tcp"
   rule_action    = "allow"
   cidr_block     = "0.0.0.0/0"
   from_port      = 443
   to_port        = 443
-}
-
-resource "aws_network_acl_rule" "ephemeral_out" {
-  network_acl_id = "${aws_network_acl.private.id}"
-  rule_number    = 110
-  egress         = true
-  protocol       = "tcp"
-  rule_action    = "allow"
-  cidr_block     = "0.0.0.0/0"
-  from_port      = 1024
-  to_port        = 65535
 }
 
 resource "aws_network_acl_rule" "ephemeral_in" {
@@ -219,6 +308,17 @@ resource "aws_network_acl_rule" "ephemeral_in" {
   to_port        = 65535
 }
 
+resource "aws_network_acl_rule" "private_http_in" {
+  network_acl_id = "${aws_network_acl.private.id}"
+  rule_number    = 101
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 80
+  to_port        = 80
+}
+
 resource "aws_network_acl_rule" "https_in" {
   network_acl_id = "${aws_network_acl.private.id}"
   rule_number    = 102
@@ -228,4 +328,94 @@ resource "aws_network_acl_rule" "https_in" {
   cidr_block     = "0.0.0.0/0"
   from_port      = 443
   to_port        = 443
+}
+
+# Docker needs tcp 2377 and 7946, and udp 7946 and 4789
+# between each node in the swarm
+resource "aws_network_acl_rule" "private_docker_in_1" {
+  network_acl_id = "${aws_network_acl.private.id}"
+  rule_number    = 200
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.public.cidr_block}"
+  from_port      = 2377
+  to_port        = 2377
+}
+
+resource "aws_network_acl_rule" "private_docker_in_2" {
+  network_acl_id = "${aws_network_acl.private.id}"
+  rule_number    = 201
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.public.cidr_block}"
+  from_port      = 7946
+  to_port        = 7946
+}
+
+resource "aws_network_acl_rule" "private_docker_in_3" {
+  network_acl_id = "${aws_network_acl.private.id}"
+  rule_number    = 202
+  egress         = false
+  protocol       = "udp"
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.public.cidr_block}"
+  from_port      = 7946
+  to_port        = 7946
+}
+
+resource "aws_network_acl_rule" "private_docker_in_4" {
+  network_acl_id = "${aws_network_acl.private.id}"
+  rule_number    = 203
+  egress         = false
+  protocol       = "udp"
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.public.cidr_block}"
+  from_port      = 4789
+  to_port        = 4789
+}
+
+resource "aws_network_acl_rule" "private_docker_out_1" {
+  network_acl_id = "${aws_network_acl.private.id}"
+  rule_number    = 200
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.public.cidr_block}"
+  from_port      = 2377
+  to_port        = 2377
+}
+
+resource "aws_network_acl_rule" "private_docker_out_2" {
+  network_acl_id = "${aws_network_acl.private.id}"
+  rule_number    = 201
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.public.cidr_block}"
+  from_port      = 7946
+  to_port        = 7946
+}
+
+resource "aws_network_acl_rule" "private_docker_out_3" {
+  network_acl_id = "${aws_network_acl.private.id}"
+  rule_number    = 202
+  egress         = true
+  protocol       = "udp"
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.public.cidr_block}"
+  from_port      = 7946
+  to_port        = 7946
+}
+
+resource "aws_network_acl_rule" "private_docker_out_4" {
+  network_acl_id = "${aws_network_acl.private.id}"
+  rule_number    = 203
+  egress         = true
+  protocol       = "udp"
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.public.cidr_block}"
+  from_port      = 4789
+  to_port        = 4789
 }
